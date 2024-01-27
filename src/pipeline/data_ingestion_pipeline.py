4c5ddf8858cafc2c3ca92159bed75be40392b534
kiran narayan <kiran.narayan71@gmail.com>
2024-01-27 22:55:43 +0530

    ingestion pipeline

---
+++ src/pipeline/data_ingestion_pipeline.py
@@ -0,0 +1,22 @@
+from src.custom_logger import logger
+from src.config import ConfigurationManager
+from src.components.data_ingestion import DataIngestion
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass    
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_ingestion_config = config.get_data_ingestion_config()
+            
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+           
+            logger.info(f"data ingestion successful")
+
+        except Exception as error:
+            logger.error(f"error downloading data: {error}")
+            

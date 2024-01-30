305f69c9796e9db080173344d1f805a0a4cb77f2
kiran narayan <kiran.narayan71@gmail.com>
2024-01-30 08:28:19 +0530

    completed data validation

--- src/components/data_validation.py
+++ src/components/data_validation.py
@@ -1,30 +1,48 @@
-import os
+## components
 import glob
 import pandas as pd
+import os
 
 from src.custom_logger import logger
 from src.entity import DataValidationConfig
 
 class DataValidation:
     def __init__(self, config: DataValidationConfig):
-        self.config = config        
+        self.config = config
+        print(self.config)
         self.data_filepath = glob.glob(os.path.join(self.config.root_dir, '*.csv'))[0]
-        
+        print(self.data_filepath)
+
     def initiate_validation(self):
         try:
             
             df = pd.read_csv(self.data_filepath)            
             col_names = self.config.schema.keys()
 
-            df.drop(self.config.columns_to_drop, inplace=True)
+            df['NoOfYears']=2024 - df['Year']
 
+
+            df.drop(self.config.columns_to_drop.name, axis=1, inplace=True)
+
+            ##checking for number of columns
             if len(df.columns) != len(col_names):
-                print(f"invalid number of columns")
+                logger.info(f"invalid number of columns")
+                return 
                 
-                print([i for i in df.columns if i not in col_names])
-
-                print(len(df.columns), len(col_names))
+            ## checking for data types of the column
+            if not (all([df[col].dtype == dtype for col, dtype in self.config.schema.items()])):
+                logger.info(f"invalid datatype for the columns")
+                return 
             
+            ## checking for null values in the dataframe
+            if not all(df.isna()):
+                logger.info(f"there are missing values in the dataframe")
+                logger.info(f"please handle the missing values before proceeding")
+
+            
+            os.makedirs(self.config.clean_dir, exist_ok=True)
+
+            df.to_csv(f'{self.config.clean_dir}//cleande_data.csv')
             
         except Exception as error:
             logger.error(f"error while validating data: {error}")

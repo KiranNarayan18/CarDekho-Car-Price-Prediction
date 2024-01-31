529bd98fe691b94b089050c15231334977023580
kiran narayan <kiran.narayan71@gmail.com>
2024-01-31 07:59:32 +0530

    validation pipeline

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+
+from src.custom_logger import logger
+
+
 from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.pipeline.data_validation_pipeline import DataValidationPipeline
 
 
 if __name__ == '__main__':
+
+    STAGE_NAME = 'DATA_INGESTION'        
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionPipeline()
     obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+    STAGE_NAME = 'DATA_VALIDATION'        
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+
+    
+  
---
+++ src/pipeline/data_validation_pipeline.py
@@ -0,0 +1,26 @@
+
+from src.custom_logger import logger
+from src.config.configuration import ConfigurationManager
+from src.components.data_validation import DataValidator
+
+class DataValidationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            obj = ConfigurationManager()
+            res = obj.get_data_validation_config()
+
+            obj2 = DataValidator(res)
+            obj2.initiate_validation()
+
+        except Exception as error:
+            logger.error(f"Failed to validate data: {error}")
+
+
+if __name__ == '__main__':
+    obj = DataValidationPipeline()
+    obj.main()
+
+    

8b4c3b051a0ec1fbf5200800bfa96d0fcb2f2084
kiran narayan <kiran.narayan71@gmail.com>
2024-02-02 08:09:45 +0530

    fix: training

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from src.custom_logger import logger
 from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
 from src.pipeline.data_validation_pipeline import DataValidationPipeline
 from src.pipeline.data_transformation_pipeline import DataTransformationPipeline
+from src.pipeline.model_training_pipeline import ModelTrainingPipeline
 
 
 if __name__ == '__main__':
@@ -30,6 +31,12 @@ if __name__ == '__main__':
     STAGE_NAME = "DATA_TRANSFORMATION"      
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataTransformationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    
+    STAGE_NAME = "MODEL_TRAINING"      
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     
--- src/pipeline/model_training_pipeline.py
+++ src/pipeline/model_training_pipeline.py
@@ -4,10 +4,25 @@ from src.config.configuration import ConfigurationManager
 from src.components.model_training import ModelTraininer
 
 
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+        try:
+            obj = ConfigurationManager()
+            obj = obj.get_model_training_config()
+
+            obj1 = ModelTraininer(obj)
+            obj1.initiate_model_training()
+
+        except Exception as error:
+            logger.error(f"Error creating model: {error}")
+
+
+
 if __name__ == '__main__':
-
-    obj = ConfigurationManager()
-    obj = obj.get_model_training_config()
-
-    obj1 = ModelTraininer(obj)
-    obj1.initiate_model_training()
+    obj = ModelTrainingPipeline()
+    obj.main()
+    
